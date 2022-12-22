39d19c6616b4967e5cceed52c81a25416833459c
karenhuang925 <92420431+karenhuang925@users.noreply.github.com>
2022-12-21 21:59:31 -0800

    navbar finish

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,19 +2,76 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
   return (
     <nav>
+      <div className='header'>
+        <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Etsy_logo.svg/2560px-Etsy_logo.svg.png'/>
+        <input className='searchBar' placeholder='Search for anything'></input>
+        <button className='searchButton'>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+        <NavLink to='/login' exact={true} className='signIn' activeClassName='active'>Sign in</NavLink>
+        <NavLink to='/carts' exact={true} className='shoppingCart' activeClassName='active'>
+          <i class="fa-solid fa-cart-shopping"></i>
+        </NavLink>
+      </div>
+
+      <ul className='headerCategory'>
+        <li className='eachCategory'>
+          <NavLink to='/categories/holiday' exact={true}>
+            Holiday Shop
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/jewelry' exact={true}>
+            Jewerlry & Accessories
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/clothing' exact={true}>
+            Clothing & Shoes
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/home' exact={true}>
+            Home & Living
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/wedding' exact={true}>
+            Wedding & Party
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/toy' exact={true}>
+            Toy & Entertainment
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/Art' exact={true}>
+            Art & Collectibles
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/craft' exact={true}>
+            Craft Supplies
+          </NavLink>
+        </li>
+        <li className='eachCategory'>
+          <NavLink to='/categories/gift' exact={true}>
+            Gift & Gift Cards
+          </NavLink>
+        </li>
+      </ul>
+
+
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
           </NavLink>
         </li>
         <li>
