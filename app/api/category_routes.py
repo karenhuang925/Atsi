0199756645815c9d346a69375368e114a4360ea7
karenhuang925 <92420431+karenhuang925@users.noreply.github.com>
2023-08-25 23:06:56 -0700

    update category api

--- app/api/category_routes.py
+++ app/api/category_routes.py
@@ -9,10 +9,16 @@ category_routes = Blueprint('categories', __name__)
 
 @category_routes.route('/<name>/products')
 def products(name):
+    """
+    query all product in that category
+    """
     products = Product.query.join(Category).filter(Category.name.ilike(f'%{name}%')).all()
     return {'Products': [product.to_dict_category() for product in products]}
 
 @category_routes.route('/')
 def categories():
+    """
+    query all category
+    """
     categories = Category.query.all()
     return {'Category': [category.to_dict() for category in categories]}
